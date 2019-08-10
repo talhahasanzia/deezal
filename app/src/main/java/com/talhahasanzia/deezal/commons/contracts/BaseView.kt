52266659a3f5fcd4769d0b92a8db7c7995867085
Talha <talhahasanzia@hotmail.com>
2019-08-10 21:42:35 +0500

    Added BaseActivity with inject() and getLayout() calls and showError() implementation qq :q :qw exit
    

--- app/src/main/java/com/talhahasanzia/deezal/commons/contracts/BaseView.kt
+++ app/src/main/java/com/talhahasanzia/deezal/commons/contracts/BaseView.kt
@@ -1,5 +1,7 @@
 package com.talhahasanzia.deezal.commons.contracts
 
+import android.support.annotation.LayoutRes
+
 /**
  * View (VIPER) that will hold common signatures for all Views
  * This will allow certain methods to be called from Presenter without requiring instance of activity
@@ -10,6 +12,12 @@ interface BaseView {
      * Simple contract that enforces views to inject all the dependencies
      */
     fun inject()
+
+    /**
+     * Get xml reference so oncreate boiler plate is not written everytime
+     */
+    @LayoutRes
+    fun getLayout() : Int
 
     /**
      * Simple error pop-up
---
+++ app/src/main/java/com/talhahasanzia/deezal/commons/ui/BaseActivity.kt
@@ -0,0 +1,33 @@
+package com.talhahasanzia.deezal.commons.ui
+
+import android.content.DialogInterface
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import com.talhahasanzia.deezal.R
+import com.talhahasanzia.deezal.commons.contracts.BaseView
+
+/**
+ * Since BaseActivity can implement BaseView, it can provide some boiler plate logic like error pop-up.
+ * This implementation is available to any child, but more importantly this is available to presenter
+ * without knowing where the implementation is coming from.
+ *
+ * getLayout() and inject() are called here in onCreate() so child class can provide layout and inject implementation
+ * without implementing onCreate() again.
+ */
+abstract class BaseActivity : AppCompatActivity(), BaseView {
+    final override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayout())
+        inject()
+    }
+
+    override fun showError(message: String) {
+        val dialog = AlertDialog.Builder(this)
+            .setMessage(message)
+            .setPositiveButton(R.string.ok) { dialogInterface, i -> dialogInterface.dismiss() }
+            .create()
+
+        dialog.show()
+    }
+}
