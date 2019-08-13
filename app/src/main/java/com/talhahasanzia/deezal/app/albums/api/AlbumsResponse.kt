5c1554ba2c879453b7c0eaedf1ecedc78cb9a22f
Talha <talhahasanzia@hotmail.com>
2019-08-13 17:10:05 +0500

    Removed dummy response pasted inside Album Response class

--- app/src/main/java/com/talhahasanzia/deezal/app/albums/api/AlbumsResponse.kt
+++ app/src/main/java/com/talhahasanzia/deezal/app/albums/api/AlbumsResponse.kt
@@ -26,21 +26,3 @@ data class Album(
     var explicit_lyrics: String,
     var type: String
 ) : Serializable
-
-/*
-* "id": "14346300",
-      "title": "Campaign Speech",
-      "link": "https://www.deezer.com/album/14346300",
-      "cover": "https://api.deezer.com/album/14346300/image",
-      "cover_small": "https://cdns-images.dzcdn.net/images/cover/0505b7f25dd3b52039cf279e6c7f2f0a/56x56-000000-80-0-0.jpg",
-      "cover_medium": "https://cdns-images.dzcdn.net/images/cover/0505b7f25dd3b52039cf279e6c7f2f0a/250x250-000000-80-0-0.jpg",
-      "cover_big": "https://cdns-images.dzcdn.net/images/cover/0505b7f25dd3b52039cf279e6c7f2f0a/500x500-000000-80-0-0.jpg",
-      "cover_xl": "https://cdns-images.dzcdn.net/images/cover/0505b7f25dd3b52039cf279e6c7f2f0a/1000x1000-000000-80-0-0.jpg",
-      "genre_id": 116,
-      "fans": 7214,
-      "release_date": "2016-10-19",
-      "record_type": "single",
-      "tracklist": "https://api.deezer.com/album/14346300/tracks",
-      "explicit_lyrics": true,
-      "type": "album"
-* */
